b60857d4eff738c8502189c359cb1b419751947d
Santiago Mamani <santiagojmamani@gmail.com>
2021-09-02 23:39:46 -0300

    Agrega pantalla "Cómo protegemos tu información"

--- lib/app/screens/commons/custom_fab_list.dart
+++ lib/app/screens/commons/custom_fab_list.dart
@@ -6,18 +6,22 @@ class CustomFABList extends StatelessWidget {
   CustomFABList({
     Key? key,
     required this.children,
+    this.mainAxisAlignment = MainAxisAlignment.end,
   })  : assert(children.length <= 2, 'La cantidad máxima de FAB son dos'),
         assert(children.isNotEmpty),
         super(key: key);
 
   final List<CustomFloatingActionButton> children;
 
+  /// Just if there is only one CustomFloatingActionButton in the children array
+  final MainAxisAlignment mainAxisAlignment;
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: children.length == 2
           ? MainAxisAlignment.spaceBetween
-          : MainAxisAlignment.end,
+          : mainAxisAlignment,
       children: children,
     );
   }
